e0edd43090149a0da6f8398cd88afe61c3ac43de
MIRKYRII <91189178+MIRKYRII@users.noreply.github.com>
2022-03-18 09:40:45 +0300

    Update Яшин Лр16п2.cpp
    поправил формулу чтобы соответствовала заданию

--- lab16/Яшин Лр16п2.cpp
+++ lab16/Яшин Лр16п2.cpp
@@ -4,15 +4,17 @@ using namespace std;
 
 int main()
 {
-    unsigned int num,b;
-    cout << "Enter massive size: ";
+    unsigned int num,b,a;
+    cout << "Введите размер массива ";
     cin >> num;
-    cout << "Enter geometry progression base: ";
+    cout << "Введите первое число прогрессии ";
     cin >> b;
+    cout << "Введите основание геометрической прогрессии ";
+    cin >> a;
+    cout<<b<<" ";
     int *p_darr = new int[num];//выделение памяти
     for (int i = 1; i < num; i++) {
-
-        p_darr[i] = pow(b,i);
+        p_darr[i] = b*pow(a,i);
         cout<<p_darr[i]<<" ";
     }
     delete [] p_darr;//очистка памяти
